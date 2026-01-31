083f11768d39b71a239089d77f1b6df9d11ad2bd
Sameer Nadaf <nsameernadaf@gmail.com>
2026-01-31 21:04:42 +0530

    Initial Commit

---
+++ RazorpayIntegrationDemo/RazorpayIntegrationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RazorpayIntegrationDemoApp.swift
+//  RazorpayIntegrationDemo
+//
+//  Created by SameerNadaf on 31/01/26.
+//
+
+import SwiftUI
+
+@main
+struct RazorpayIntegrationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
